3a081bec4d0e3ddb00b458d9b0dff81df99043ff
muthukumaran2 <muthukumaran@edu.chat>
2016-08-28 02:05:52 -0400

    background image added, optimized skillchart to sync with it.

--- js/loading.js
+++ js/loading.js
@@ -1,7 +1,9 @@
 $(function(){
+      // Initially make home active, load middlebox.html and alert User
       $('#home').addClass('active');
       $("#middle").load("html/middlebox.html");
       alert("For best experience please view the site in fullscreen Desktop environment. Mobile support coming soon...");
+      // Onclick Skills goto skills.html
       $("#skills").click(function(){
         $("#home").removeClass('active');
         $("#myprofile").addClass('active');
@@ -12,24 +14,25 @@ $(function(){
         });
 
   	  });
+      // Onclick Home goto home.html
       $("#home").click(function(){
         $("#myprofile").removeClass('active');
         $("#home").addClass('active');
         $("#middle").load("html/middlebox.html");
       });
+      // Onclick brand name goto home.html
       $("#brand").click(function(){
         $("#myprofile").removeClass('active');
         $("#home").addClass('active');
         $("#middle").load("html/middlebox.html");
       });
+      // Onclick brand image goto home.html
       $("#brand_img").click(function(){
         $("#myprofile").removeClass('active');
         $("#home").addClass('active');
         $("#middle").load("html/middlebox.html");
       });
-      $("#image").click(function(){
-        $('#github').tooltip('close');
-      });
+      // Onclick Interests goto interests.html
       $("#interests").click(function(){
         $("#home").removeClass('active');
         $("#myprofile").addClass('active');
@@ -39,16 +42,26 @@ $(function(){
            });
         });
       });
+      // Onclick Experience goto experience.html
       $("#experience").click(function(){
         $("#home").removeClass('active');
         $("#myprofile").addClass('active');
         $("#middle").load("html/experience.html");
             
       });
+      // Onclick Projects goto projects.html
       $("#projects").click(function(){
         $("#home").removeClass('active');
         $("#myprofile").addClass('active');
         $("#middle").load("html/projects.html");
+      });
+      // Script for tooltip(hint while hoverin social network images)
+      $("[data-toggle = 'tooltip']").tooltip({
+          trigger : 'hover'
+      });
+      // Onclick social networking icons close the tooltip
+      $("#image").click(function(){
+        $('#github').tooltip('close');
       });
 });
 
